c98795728a70b126654a4e522a4100468bca071e
Marcin Boroń <marcin.boron@gmail.com>
2024-10-31 20:28:52 +0100

    App skeleton

---
+++ tests/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_example(): void
+    {
+        self::assertTrue(true);
+    }
+}
